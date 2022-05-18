c2262f3dd6f31f1d40587b841c7d934ff27406a7
Roald Ong <roald.ong@gmail.com>
2022-05-18 21:34:59 +0800

    Added in react-router as a dependency

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import WordInput from "./WordInput";
@@ -8,9 +9,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<WordInput />
-	</React.StrictMode>
+	<Router>
+		<Routes>
+			<Route path="/" element={<WordInput />} />
+		</Routes>
+	</Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
